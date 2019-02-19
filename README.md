3395ecd1ba8a0cc3f25f73bec19455e0b3e89f08
Pangsui Usifu Linge <upangsuilingeh@gmail.com>
2019-02-19 15:04:53 +0100

    Add .

---
+++ README.md
@@ -0,0 +1,1 @@
+This code is bassically for the creation of an etch-a-sketch.All the explanatory comments have been put beside each line of code for self explanation.The link to it is at:
---
+++ main.js
@@ -0,0 +1,46 @@
+// function that builds a grid in the "container"
+function createGrid(x) {
+    for (var rows = 0; rows < x; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+            const container = document.querySelector('#container');
+            const content = document.createElement('div');  //could also be written as   $("#container").append("<div class='grid'></div>");
+            content.classList.add('grid');
+            container.appendChild(content);
+        };
+    };
+    $(".grid").width(960/x);
+    $(".grid").height(960/x);  //this gives the uniform dimension of each grid created
+};
+
+// function that clears the grid
+function clearGrid(){
+    $(".grid").remove();
+};  
+
+// function that prompts the user to select the number of boxes in a new grid
+// the function then also creates that new grid
+function refreshGrid(){
+    var z = prompt("How many boxes per side?");
+    clearGrid();
+    createGrid(z);
+};
+
+// create a 16x16 grid when the page loads
+// creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
+// allows the click of a button to prompt the user to create a new grid
+$(document).ready(function() {
+    createGrid(16);
+
+    $(".grid").mouseover(function() {
+        $(this).css("background-color", "blue");
+        });
+
+    $(".newGrid").click(function() {
+        refreshGrid();
+
+        $(".grid").mouseover(function() {
+        $(this).css("background-color", "red");
+        });
+    });
+});
+
